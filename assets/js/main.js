ae632d2d87b84dd1eab8e8d3d4cde1f3f99bdbad
ItzPro <zzombie774@gmail.com>
2024-05-20 00:44:20 -0600

    Actualizacion
    Contactame funcional con envio de correo funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,5 +36,49 @@ tabs.forEach((tab) => {
     });
 });
 
-
 /*=============== CONTACT FORM =============== */
+
+const contactForm = document.getElementById('contact-form'),
+      contactName = document.getElementById('contact-name'),
+      contactEmail = document.getElementById('contact-email'),
+      contactSubject = document.getElementById('contact-subject'),
+      contactMessage = document.getElementById('contact-message'),
+      errorMessage = document.getElementById('error-message'); // Asegúrate de que este ID sea correcto en tu HTML
+
+const sendEmail = (e) => {
+    e.preventDefault(); // Prevenir la recarga de la página
+
+    // Verificación de si están vacíos los campos
+    if (
+        contactName.value === '' ||
+        contactEmail.value === '' ||
+        contactSubject.value === '' ||
+        contactMessage.value === ''
+    ) {
+        errorMessage.textContent = 'Por favor llene los campos';
+    } else {
+        // Consume el servicio de envío de correo
+        emailjs.sendForm('service_5dalmkg', 'template_6fnsr3h', '#contact-form', 'lghDfaVAfg9J5-EiP')
+            .then(() => {
+                errorMessage.classList.add('color-first');
+                errorMessage.textContent = 'Mensaje Enviado';
+
+                // Remover el mensaje pasados 5 segundos
+                setTimeout(() => {
+                    errorMessage.textContent = '';
+                }, 5000);
+            }, (error) => {
+                alert('OOPs! Algo salió mal', error);
+            }
+        );
+
+        // Limpiar los inputs
+        contactName.value = '';
+        contactEmail.value = '';
+        contactSubject.value = '';
+        contactMessage.value = '';
+    }
+};
+
+// Añadir el event listener para el envío del formulario
+contactForm.addEventListener('submit', sendEmail);
